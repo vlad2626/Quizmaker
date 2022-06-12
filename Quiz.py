b7e4dea85c7c9978de216f074ac1070f3ef6d46f
vlad2626 <57768989+vlad2626@users.noreply.github.com>
2022-06-12 15:48:16 -0400

    Added questions and answers. label dynamically  populate using the dictionary .  Entry.

--- Quiz.py
+++ Quiz.py
@@ -65,6 +65,7 @@ class Quiz:
         window.rowconfigure([0,1,2,3,4], minsize=50)
         window.columnconfigure([0, 1, 2, 3,4,5,6,7,8,9], minsize=50)
 
+        #unused loop.
         # for i in range(3):
         #     window.columnconfigure(i, weight=1, minsize=75)
         #     window.rowconfigure(i, weight=1, minsize=50)
@@ -76,13 +77,12 @@ class Quiz:
         #             borderwidth=5
         #         )
         #         frame.grid(row=i,column=j, padx=5, pady=5)
-        #         # label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}", padx=5 , pady=5
+        #         # label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}", padx=5 , pady=5#
 
-        label1=tk.Label(text="QUIZ",font=30)
-        label1.grid(row=0,column =2)
+        k=tk.Label(text="QUIZ",font=30)
+        k.grid(row=0,column=3)
 
-        label1.grid(row=0,column=3)
-
+        label1=tk.Label(text="")
         label2= tk.Label(text="")
         label3 = tk.Label(text="")
         label4 = tk.Label(text="")
@@ -93,12 +93,67 @@ class Quiz:
         label9 = tk.Label(text="")
         label10 = tk.Label(text="")
         label11 = tk.Label(text="")
+        label12 = tk.Label(text="")# labels
+        # entry boxes
+        entry1=tk.Entry(tk.Entry(master=window, width=30))
+        entry2 = tk.Entry(tk.Entry(master=window, width=30))
+        entry3 = tk.Entry(tk.Entry(master=window, width=30))
+        entry4 = tk.Entry(tk.Entry(master=window, width=30))
+        entry5 = tk.Entry(tk.Entry(master=window, width=30))
+        entry6 = tk.Entry(tk.Entry(master=window, width=30))
+        entry7 = tk.Entry(tk.Entry(master=window, width=30))
+        entry8 = tk.Entry(tk.Entry(master=window, width=30))
+        entry9 = tk.Entry(tk.Entry(master=window, width=30))
+        entry10 = tk.Entry(tk.Entry(master=window, width=30))
+        entry11 = tk.Entry(tk.Entry(master=window, width=30))
 
-        l=[label2,label3,label4,label5,label6,label8,label9,label10]
 
+        values = list(questions.values())
+        keys= list(questions.keys())
+
+        quizQuestions=[]
+        quizAnswers=[]
+        l=[label1,label2,label3,label4,label5,label6,label8,label9,label10,label11,]
+        boxes=[entry1,entry2,entry3,entry4,entry5,entry6,entry7,entry8,entry9,entry10]
+
+
+
+
+
+        count1=0
+        count2=0
+        q2=""
+        trimmed=""
+
+
+        # sets the label text to the dictionary
         for i in l:# loop through the labels
-            for j in questions:
-                print(j)
+
+            q=keys[count1]
+            trimmed=q.find("?"[:len(q)])# afer question mark finds the answer
+            trimmed2 = q[:trimmed+1]  # question
+
+            finalAnswer=q[trimmed+1:len(q)-1]  # answer
+
+
+            finalQuestion=trimmed2 # question
+            # populate list with correct answerrs
+            quizQuestions.append(finalQuestion)
+            quizAnswers.append(finalAnswer)
+            i=tk.Label(text=finalQuestion)
+
+            i.grid(column=1, row= count1+1)
+            count1 += 1
+
+        print(quizAnswers)
+        print(quizQuestions)
+        # text boxes
+        for j in boxes:  # loop through the labels
+            j= tk.Entry(master=window, width=30)
+            j.grid(column=3, row=count2+1)
+            count2+=1
+
+
 
 
 
